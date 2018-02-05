9b467b1b048a72ed57aaee76d3811b36767eb328
Illai Zeevi <illai.zeevi@gmail.com>
2018-02-05 22:11:51 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engin', 'hbs');
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append file!');
+        }
+    });
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (txt) => {
+    return txt.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'illai',
+    //     things: [
+    //         'One thing',
+    //         'Tow things',
+    //         'Three things'
+    //     ]
+    // });
+
+    res.render('home.hbs', {
+        title: 'Home',
+        welcomeMessage: 'Welcome to my site'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'About'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Could not find page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
